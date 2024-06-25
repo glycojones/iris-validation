07fce071fadd3650e1adca50ded8da279018b5b9
Jon Agirre <jon.agirre@york.ac.uk>
2024-06-25 15:37:11 +0100

    Restructured source tree and fixed tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,25 +4,49 @@ from os import path
 
 from iris_validation import generate_report
 
-
-INPUT_DIR = './test_data/'
-OUTPUT_DIR = './test_output/'
+INPUT_DIR = './tests/test_data/'
+OUTPUT_DIR = './tests/test_output/'
 
 PDB_ID = '3atp'
 ROOT_PATH = str(os.path.join(INPUT_DIR, PDB_ID)) + '{suffix}'
 
 
-if __name__ == '__main__':
-    cwd = os.getcwd()
-    print (cwd)
+def test_generate_vanilla_report ():
+    t0 = time.time()
+
+    from importlib.metadata import version
+    print("Testing Iris version " + version("iris_validation"))
+          
+    generate_report(latest_model_path=ROOT_PATH.format(suffix='_final.pdb'),
+                    latest_reflections_path=ROOT_PATH.format(suffix='_final.mtz'),
+                    previous_model_path=ROOT_PATH.format(suffix='_0cyc.pdb'),
+                    previous_reflections_path=ROOT_PATH.format(suffix='_0cyc.mtz'),
+                    output_dir=OUTPUT_DIR,
+                    run_covariance=False,
+                    run_molprobity=False,
+                    calculate_rama_z=False,
+                    multiprocessing=True
+    )
+    assert path.exists(OUTPUT_DIR.format(suffix="/report.html"))
+
+    t1 = time.time()
+    print('Time taken:', round(t1-t0, 2), 's')
+    print()
+
+def test_generate_rama_z_report ():
     t0 = time.time()
 
     generate_report(latest_model_path=ROOT_PATH.format(suffix='_final.pdb'),
                     latest_reflections_path=ROOT_PATH.format(suffix='_final.mtz'),
                     previous_model_path=ROOT_PATH.format(suffix='_0cyc.pdb'),
                     previous_reflections_path=ROOT_PATH.format(suffix='_0cyc.mtz'),
-                    output_dir=OUTPUT_DIR)
-    assert path.exists(OUTPUT_DIR)
+                    output_dir=OUTPUT_DIR,
+                    run_covariance=False,
+                    run_molprobity=False,
+                    calculate_rama_z=True,
+                    multiprocessing=True
+    )
+    assert path.exists(OUTPUT_DIR.format(suffix="/report.html"))
 
     t1 = time.time()
     print('Time taken:', round(t1-t0, 2), 's')
